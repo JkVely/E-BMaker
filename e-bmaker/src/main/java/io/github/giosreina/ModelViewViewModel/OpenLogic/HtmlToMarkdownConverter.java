a9090a4a75903b691c50b8bcba45b1a49e94d37e
giosreina <giosreina@gmail.com>
2025-06-01 14:41:44 -0500

    cambio en la clase adaptadora de html a markdown

--- e-bmaker/src/main/java/io/github/giosreina/ModelViewViewModel/OpenLogic/HtmlToMarkdownConverter.java
+++ e-bmaker/src/main/java/io/github/giosreina/ModelViewViewModel/OpenLogic/HtmlToMarkdownConverter.java
@@ -1,27 +1,33 @@
 package io.github.giosreina.ModelViewViewModel.OpenLogic;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class HtmlToMarkdownConverter {
     /**
      * Convierte texto Markdown básico a HTML.
      * Soporta: **negrita**, _cursiva_, # títulos, listas y saltos de línea.
      */
-    public static String convert(String html) {
-        if (html == null) return "";
-        String markdown = html;
-        // Negrita: **texto**
-        markdown = markdown.replaceAll("<b>$1</b>","\\*\\*(.+?)\\*\\*");
-        // Cursiva: _texto_
-        markdown = markdown.replaceAll("<i>$1</i>","_(.+?)_");
-        // Títulos: # Título
-        markdown = markdown.replaceAll("<h1>$1</h1>","(?m)^# (.+)$");
-        markdown = markdown.replaceAll("<h2>$1</h2>","(?m)^## (.+)$");
-        markdown = markdown.replaceAll("<h3>$1</h3>", "(?m)^### (.+)$");
-        // Listas: - item
-        markdown = markdown.replaceAll("<li>$1</li>","(?m)^- (.+)$");
-        // Agrupar <li> en <ul>
-        markdown = markdown.replaceAll("<ul>$1</ul>\n","((<li>.+?</li>\n?)+)");
-        // Saltos de línea
-        markdown = markdown.replaceAll("<br/>", "\n");
+    public static List<String> convert(List<String> html) {
+        List<String> markdown = new ArrayList<>();
+        for(String linehtml: html) {
+            String linemarkdown = linehtml;
+            // Negrita: **texto**
+            linemarkdown = linemarkdown.replaceAll("<b>$1</b>","\\*\\*(.+?)\\*\\*");
+            // Cursiva: _texto_
+            linemarkdown = linemarkdown.replaceAll("<i>$1</i>","_(.+?)_");
+            // Títulos: # Título
+            linemarkdown = linemarkdown.replaceAll("<h1>$1</h1>","(?m)^# (.+)$");
+            linemarkdown = linemarkdown.replaceAll("<h2>$1</h2>","(?m)^## (.+)$");
+            linemarkdown = linemarkdown.replaceAll("<h3>$1</h3>", "(?m)^### (.+)$");
+            // Listas: - item
+            linemarkdown = linemarkdown.replaceAll("<li>$1</li>","(?m)^- (.+)$");
+            // Agrupar <li> en <ul>
+            linemarkdown = linemarkdown.replaceAll("<ul>$1</ul>\n","((<li>.+?</li>\n?)+)");
+            // Saltos de línea
+            linemarkdown = linemarkdown.replaceAll("<br/>", "\n");
+            markdown.add(linemarkdown);
+        }
         return markdown;
     }
 }
